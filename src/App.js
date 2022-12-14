e19862a5bf5b3b35d8c46899e3ea6fafbb53cfaa
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-13 19:12:31 -0500

    rating functionality using useState hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 
 function App(props) {
+
   const [checked, setChecked] = useState(false);
+
+  const CreateArray = (length) => [...Array(length)];
+
+  function Star({selected=false, onSelect}) {
+    return <FaStar color={selected ? "Red" : "Gray"} onClick={onSelect}/>
+  }
+
+  function StarRatiing({totalStars = 5}) {
+    const [selectedStar, setSelectedStar] = useState(0);
+    return (
+      <>
+        {CreateArray(totalStars).map((n,i)=>
+            <Star 
+              key={i} 
+              selected= {selectedStar > i} 
+              onSelect={() => setSelectedStar(i+1)}
+            />
+        )}
+        <p> {selectedStar} of {totalStars}</p>
+      </>
+      )
+  }
 
   return (
     <div>
       <h1>Learn React Hook</h1>
       <input type="checkbox" value={checked} onChange={()=>setChecked(!checked)} />
       <p>{checked ? "Checked" : "Not Checked"}</p>
+      <StarRatiing totalStars={5}/>
     </div>
   );
 }
