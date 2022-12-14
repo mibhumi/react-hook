58db3b2e71a1aa9cd7fb9a8378a897ad81aa4bd7
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-14 00:47:31 -0500

    useRef implementation

--- src/App.js
+++ src/App.js
@@ -1,49 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer, useState , useRef} from 'react';
 
 function App(props) {
 
-  const [number, setNumber] = useReducer((number, newNumber)=> number + newNumber, 0);
-  const [checked, toggle] = useReducer(
-    checked => !checked, false
-  );
+  const color = useRef();
+  const sound = useRef();
 
-  const initState = {
-    name: "Bhumi Patel",
-    counter: 0
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} and ${colorVal}`);
+    color.current.value = "";
+    sound.current.value = "";
   }
-
-  const reducer = (state, action) => {
-    // eslint-disable-next-line default-case
-    switch(action.type) {
-      case "inc":
-        return { ...state,
-          counter: state.counter + 1 
-        };
-      case "dec":
-        return { ...state,
-          counter: state.counter - 1
-        };
-    }
-  }
-
-  const [state, dispatch] = useReducer( reducer , initState);
 
   return (
     <div>
-      <h1>Learn React Hook</h1>
-      <p onClick={()=>setNumber(1)}>{number}</p>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={toggle}
-      />
-      {checked ? "I am checked" : "I am not checked"}
-      <h3>Counter craeted by {state.name}</h3>
-      <p>{state.counter}</p>
-      <button onClick={()=>dispatch({type: "inc"})}>Increment</button>
-      <button onClick={()=>dispatch({type: "dec"})}>Decrement</button>
+      <h3>Welcome to useRef!</h3>
+      <form>
+        <input ref={sound} type="text" placeholder='Sound' /><br/>
+        <input ref={color} type="color"/><br/>
+        <button onClick={submit}>Add</button>
+      </form>
     </div>
   );
 }
