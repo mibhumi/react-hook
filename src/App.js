8caad3f2d46888fc92864498aad9fa7d507232c9
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-14 01:05:27 -0500

    created custom hook for input form to setValue

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useReducer, useState , useRef} from 'react';
+import { useInput } from './useInput';
 
 function App(props) {
 
-  const color = useRef();
-  const sound = useRef();
+  // const color = useRef();
+  // const sound = useRef();
+
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#909090");
 
   const submit = (e) => {
     e.preventDefault();
-    const soundVal = sound.current.value;
-    const colorVal = color.current.value;
-    alert(`${soundVal} and ${colorVal}`);
-    color.current.value = "";
-    sound.current.value = "";
+    alert(`${titleProps.value} and ${colorProps.value}`);
+    // const soundVal = sound.current.value;
+    // const colorVal = color.current.value;
+    // alert(`${soundVal} and ${colorVal}`);
+    // color.current.value = "";
+    // sound.current.value = "";
   }
 
   return (
     <div>
       <h3>Welcome to useRef!</h3>
-      <form>
+      {/* <form>
         <input ref={sound} type="text" placeholder='Sound' /><br/>
         <input ref={color} type="color"/><br/>
+        <button onClick={submit}>Add</button>
+      </form> */}
+       <form>
+        <input 
+          {...titleProps}
+          type="text"
+          placeholder="Enter title"
+        />
+        <input 
+          type="color"
+          {...colorProps}
+          placeholder="Choose Color"
+        />
         <button onClick={submit}>Add</button>
       </form>
     </div>
