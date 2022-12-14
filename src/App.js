3c58d7f1c43605d180354a1218577c939ed13c69
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-13 19:36:47 -0500

    implement fetch user api in useEffect

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ function App(props) {
 
   const [checked, setChecked] = useState(false);
   const [name, setName] = useState("");
+  const [admin, setAdmin] = useState();
+  const [data, setData] = useState([]);
 
   const CreateArray = (length) => [...Array(length)];
 
   useEffect(()=>{
     document.title = `Celebrate ${name}`;
+    console.log(`name is: ${name}`)
   },[name]);
 
+  useEffect(()=>{
+    console.log(`admin is: ${admin}`);
+  },[admin]);
+
+  // cause data flikering
+  useEffect(()=>{
+    fetch(`https://api.github.com/users`)
+    .then(response=>response.json())
+    .then(setData);
+  },[data]);
 
   function Star({selected=false, onSelect}) {
     return <FaStar color={selected ? "Red" : "Gray"} onClick={onSelect}/>
@@ -45,6 +58,15 @@ function App(props) {
       <StarRatiing totalStars={5}/>
       <p>Congratulations {name}!</p>
       <button onClick={()=>setName("Bhumi")}>Change winner</button>
+      <button onClick={()=>setAdmin(true)}>Change Admin</button>
+      {
+        data && data.map(user=>(
+          <li key={user.id}>{user.login}</li>
+        ))
+      }
+      {
+        data && <button onClick={()=>setData([])}>Remove Data</button>
+      }
     </div>
   );
 }
