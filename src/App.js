0a16d7c71c02583bbd8e3d8f9686219da69d30aa
Bhumi Patel <bhumipatel1196@gmail.com>
2022-12-13 19:17:45 -0500

    implement useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 
 function App(props) {
 
   const [checked, setChecked] = useState(false);
+  const [name, setName] = useState("");
 
   const CreateArray = (length) => [...Array(length)];
+
+  useEffect(()=>{
+    document.title = `Celebrate ${name}`;
+  },[name]);
+
 
   function Star({selected=false, onSelect}) {
     return <FaStar color={selected ? "Red" : "Gray"} onClick={onSelect}/>
   }
 
   function StarRatiing({totalStars = 5}) {
+
     const [selectedStar, setSelectedStar] = useState(0);
+
     return (
       <>
         {CreateArray(totalStars).map((n,i)=>
@@ -35,6 +43,8 @@ function App(props) {
       <input type="checkbox" value={checked} onChange={()=>setChecked(!checked)} />
       <p>{checked ? "Checked" : "Not Checked"}</p>
       <StarRatiing totalStars={5}/>
+      <p>Congratulations {name}!</p>
+      <button onClick={()=>setName("Bhumi")}>Change winner</button>
     </div>
   );
 }
